7a3a59170b13a46a72d5655b5919f92b0b16038c
DenysZaruba <deniszaruba03@gmail.com>
2023-03-21 13:40:51 +0200

    Denys.zaruba animation

--- src/services/config-loader.ts
+++ src/services/config-loader.ts
@@ -1,50 +1,47 @@
 import IAnimationConfig from "../models/animation-config";
 import { IConfigLoaderService } from "./config-loader-interface";
 import { AndriiSlobodianiukConfigLoaderService } from "./students/andrii-slobodianiuk";
-import { OleksandrZhukConfigLoaderService } from "./students/oleksandr-zhuk"; 
-import { YaroslavPasichnykConfigLoaderService } from "./students/yaroslav-pasichnyk"; 
+import { OleksandrZhukConfigLoaderService } from "./students/oleksandr-zhuk";
+import { YaroslavPasichnykConfigLoaderService } from "./students/yaroslav-pasichnyk";
 import { YaroslavTsvykConfigLoaderService } from "./students/yaroslav-tsvyk";
 import { OstapBlystsivConfigLoaderService } from "./students/ostap-blystsiv";
+import { DenysZarubaConfigLoaderService } from "./students/denys-zaruba";
 
 export class MainConfigLoaderService {
+  private services: IConfigLoaderService[];
 
-    private services: IConfigLoaderService[];
+  constructor() {
+    this.services = [
+      new AndriiSlobodianiukConfigLoaderService(),
+      new OleksandrZhukConfigLoaderService(),
+      new YaroslavPasichnykConfigLoaderService(),
+      new YaroslavTsvykConfigLoaderService(),
+      new OstapBlystsivConfigLoaderService(),
+      new DenysZarubaConfigLoaderService(),
+    ];
+  }
 
-    constructor() {
-        this.services = [
-            new AndriiSlobodianiukConfigLoaderService(),
-            new OleksandrZhukConfigLoaderService(),
-            new YaroslavPasichnykConfigLoaderService(),
-            new YaroslavTsvykConfigLoaderService(),
-            new OstapBlystsivConfigLoaderService()
-        ];
+  public loadAllConfigs(): IAnimationConfig[] {
+    const results = [];
+    let counter = 0;
+
+    for (const service of this.services) {
+      let name = "";
+      let email = "";
+
+      try {
+        name = service.getStudentName();
+        email = service.getStudentEmail();
+        const config = service.loadConfig();
+        config.id = counter.toString();
+
+        results.push(config);
+        counter++;
+      } catch (exc) {
+        console.error(`Error occured while processing student '${name}' - ${email}.`, exc);
+      }
     }
 
-    public loadAllConfigs(): IAnimationConfig[] {
-
-        const results = [];
-        let counter = 0;
-
-        for (const service of this.services) {
-
-            let name = '';
-            let email = '';
-
-            try {
-
-                name = service.getStudentName();
-                email = service.getStudentEmail();
-                const config = service.loadConfig();
-                config.id = counter.toString();
-
-                results.push(config);
-                counter++;
-
-            } catch (exc) {
-                console.error(`Error occured while processing student '${name}' - ${email}.`, exc);
-            }
-        }
-
-        return results;
-    }
+    return results;
+  }
 }
