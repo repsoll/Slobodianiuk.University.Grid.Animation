920edcc466aaa38593455d70e0af058aa7022c1f
JLW157 <alexbiletskyi17@gmail.com>
2023-03-22 22:25:32 +0200

    animation added

--- src/services/config-loader.ts
+++ src/services/config-loader.ts
@@ -21,6 +21,7 @@ import { OleksandrZhovanukConfigLoaderService } from "./students/oleksandr-zhova
 import { BohdanDzirbaConfigLoaderService } from "./students/bohdan-dzirba";
 import { YaroslavHolovkoConfigLoaderService } from "./students/yaroslav-holovko";
 import { BardakovConfigLoaderService } from "./students/bardakov";
+import { OlesandrBiletskyiConfigLoaderService } from "./students/oleksandr-biletskyi";
 
 export class MainConfigLoaderService {
   private services: IConfigLoaderService[];
@@ -47,7 +48,8 @@ export class MainConfigLoaderService {
       new BohdanDzirbaConfigLoaderService(),
       new MalishVitaliyConfigLoaderService(),
       new YaroslavHolovkoConfigLoaderService(),
-      new BardakovConfigLoaderService()
+      new BardakovConfigLoaderService(),
+      new OlesandrBiletskyiConfigLoaderService()
     ];
   }
 
