5ebc041e5943556a7a9f57a04d9a194426e40f0d
nekleevaN <nekleeva@gmail.com>
2024-05-07 04:36:57 -0700

    Add Anastasia.Dmytryshyn animation

--- src/services/config-loader.ts
+++ src/services/config-loader.ts
@@ -40,6 +40,7 @@ import { DmytroSerafymConfigLoaderService } from "./students/dmytro_serafym";
 import { VictoriaKondratskaConfigLoaderService } from "./students/victoria-kondratska";
 import { AndriiKozynyiConfigLoaderService } from "./students/andrii-kozynyi";
 import { MariiaHusakConfigLoaderService } from "./students/mariia-husak";
+import { AnastasiaDmytryshynConfigLoaderService } from "./students/anastasia-dmytryshyn";
 
 export class MainConfigLoaderService {
   private services: IConfigLoaderService[];
@@ -86,6 +87,7 @@ export class MainConfigLoaderService {
       new VictoriaKondratskaConfigLoaderService(),
       new AndriiKozynyiConfigLoaderService(),
       new MariiaHusakConfigLoaderService(),
+      new AnastasiaDmytryshynConfigLoaderService(),
     ];
   }
 
